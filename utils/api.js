7f5e81e6862c7148e60c0a0e12a2e36dc1dde301
QuantumSnow0 <bmuthuri93@gmail.com>
2025-06-30 14:34:42 +0300

    added search and carousel

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-//export const api = "https://sokoni-backend-uvp5.onrender.com";
+export const api = "https://sokoni-backend-uvp5.onrender.com";
 
-export const api = "https://c38d-154-159-198-16.ngrok-free.app";
+//export const api = "https://c38d-154-159-198-16.ngrok-free.app";
 //export const api = "https://4290-102-223-33-177.ngrok-free.app";
